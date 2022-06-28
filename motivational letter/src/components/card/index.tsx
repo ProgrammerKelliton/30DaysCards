39bb3e262d65c712870860e00583c16a869eb177
Kelliton <programmerKelliton@gmail.com>
2022-06-28 16:07:21 -0300

    Adicionando package de api

--- motivational letter/src/components/card/index.tsx
+++ motivational letter/src/components/card/index.tsx
@@ -1,32 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./style.css";
 
 type propsType = {
     name: string;
 };
 
-type Data = {
-    id: number;
-    advice: string;
-};
-
 export function Card(props: propsType) {
-    let [message, setMessage] = useState<Data>();
-
-    useEffect(() => {
-        async function GetMessage() {
-            const resp = await fetch("https://api.adviceslip.com/advice");
-            const data = await resp.json();
-
-            setMessage(data.slip);
-        }
-        GetMessage();
-    }, []);
-
     return (
         <div className="card">
             <h1 className="card__title">{props.name}</h1>
-            <p className="card__message">{message?.advice}</p>
+            <p className="card__message">{}</p>
         </div>
     );
 }
