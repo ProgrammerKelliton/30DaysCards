fddee68fa4fccf9e142b7626ee385bdaa1b0c556
Kelliton <programmerKelliton@gmail.com>
2022-06-28 19:29:46 -0300

    Adicionando cards por chamada

--- motivational letter/src/App.tsx
+++ motivational letter/src/App.tsx
@@ -1,33 +1,38 @@
 import { Card } from "./components/card";
 import "./App.css";
-import { client } from "./services/client";
-import { gql } from "@apollo/client";
-import { useEffect, useState } from "react";
+
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
+
+const card = gql`
+    query MyQuery {
+        cards {
+            dia
+            descricao
+        }
+    }
+`;
+
+const Cards = ({ children }: any) => <div>{children}</div>;
 
 function App() {
-    useEffect(() => {
-        function StartAPI() {
-            client
-                .query({
-                    query: gql`
-                        query MyQuery {
-                            cards {
-                                dia
-                                descricao
-                            }
-                        }
-                    `,
-                })
-                .then((resp) => console.log(resp.data));
-        }
-        StartAPI();
-    }, []);
+    const { loading, data } = useQuery(card);
 
-    return (
-        <div>
-            <Card name="Name here"></Card>
-        </div>
-    );
+    if (loading) {
+        return <Cards>Carregando</Cards>;
+    } else {
+        return (
+            <Cards>
+                {data.cards.map((item: any, key: number) => (
+                    <Card
+                        name={item.dia}
+                        key={key}
+                        message={item.descricao}
+                    ></Card>
+                ))}
+            </Cards>
+        );
+    }
 }
 
 export default App;
--- motivational letter/src/main.tsx
+++ motivational letter/src/main.tsx
@@ -1,8 +1,8 @@
-import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { client } from "./services/client";
+import { ApolloProvider } from "@apollo/client";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
