f3b3f8d975b2ef0121b5ada89f3d5a7126738196
Kelliton <programmerKelliton@gmail.com>
2022-06-28 16:14:48 -0300

    Movendo arquivos

---
+++ motivational letter/src/services/client.js
@@ -0,0 +1,8 @@
+import { ApolloClient } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://api-sa-east-1.graphcms.com/v2/cl4ycfyto186y01ta8svaa8z4/master",
+    cache: new InMemoryCache(),
+});
+
+export default client;
