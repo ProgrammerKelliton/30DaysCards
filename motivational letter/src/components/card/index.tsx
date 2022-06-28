ef8a1d1a06de115e11d76ab04994f72514212b10
Kelliton <programmerKelliton@gmail.com>
2022-06-28 17:40:06 -0300

    Adicionando api

--- motivational letter/src/components/card/index.tsx
+++ motivational letter/src/components/card/index.tsx
@@ -3,13 +3,14 @@ import "./style.css";
 
 type propsType = {
     name: string;
+    message?: string;
 };
 
 export function Card(props: propsType) {
     return (
         <div className="card">
             <h1 className="card__title">{props.name}</h1>
-            <p className="card__message">{}</p>
+            <p className="card__message">{props.message}</p>
         </div>
     );
 }
---
+++ motivational letter/src/services/client.tsx
@@ -0,0 +1,17 @@
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: "https://api-sa-east-1.graphcms.com/v2/cl4ycfyto186y01ta8svaa8z4/master",
+    cache: new InMemoryCache(),
+});
+
+client.query({
+    query: gql`
+        query MyQuery {
+            cards {
+                dia
+                descricao
+            }
+        }
+    `,
+});
