d300f07be07d408f6bed0e8430872d52ea5fc7f3
Kelliton <programmerKelliton@gmail.com>
2022-06-28 12:57:55 -0300

    Modificando arquivos

--- motivational letter/src/components/card/index.tsx
+++ motivational letter/src/components/card/index.tsx
@@ -5,13 +5,20 @@ type propsType = {
     name: string;
 };
 
+type Data = {
+    id: number;
+    advice: string;
+};
+
 export function Card(props: propsType) {
-    let [message, setMessage] = useState("");
+    let [message, setMessage] = useState<Data>();
 
     useEffect(() => {
         async function GetMessage() {
             const resp = await fetch("https://api.adviceslip.com/advice");
             const data = await resp.json();
+
+            setMessage(data.slip);
         }
         GetMessage();
     }, []);
@@ -19,7 +26,7 @@ export function Card(props: propsType) {
     return (
         <div className="card">
             <h1 className="card__title">{props.name}</h1>
-            <p className="card__message">{message}</p>
+            <p className="card__message">{message?.advice}</p>
         </div>
     );
 }
