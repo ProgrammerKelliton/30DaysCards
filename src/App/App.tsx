62db988149fee1a740e04b5f4a878eedaa873e96
Kelliton <programmerKelliton@gmail.com>
2022-06-30 11:53:40 -0300

    Ajustando css

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { gql, useQuery } from "@apollo/client";
 import CreateElement from "../tsx/ElementCreate";
-import { Card } from "../components/card";
 
 const card = gql`
     query MyQuery {
@@ -15,7 +14,7 @@ const card = gql`
 function App() {
     let { loading, data } = useQuery(card);
     if (loading) {
-        return <h1>Carregando</h1>;
+        return <h1 className="loading__title">Carregando</h1>;
     } else {
         return (
             <div className="App">
